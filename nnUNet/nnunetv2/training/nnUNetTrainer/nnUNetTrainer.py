8248d066a136627602d1416435c70718a98614c8
Zeineb Ben Chaaben <benchaaben@RAD-GPU-ADAMS-1.rad.mri.tu-muenchen.de>
2025-11-03 10:06:31 +0100

    fix inference script

--- nnUNet/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnUNet/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -143,19 +143,19 @@ class nnUNetTrainer(object):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        """
-        self.initial_lr = 1e-5
+        
+        self.initial_lr = 3e-4
         self.weight_decay = 1e-05
         self.oversample_foreground_percent = 0.33
         self.probabilistic_oversampling = False
-        self.num_iterations_per_epoch = 300
-        self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 100
+        self.num_iterations_per_epoch = 350
+        self.num_val_iterations_per_epoch = 75
+        self.num_epochs = 200
         self.current_epoch = 0
         self.enable_deep_supervision = True
-        """
-
         
+
+        """
         self.initial_lr = 1e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
@@ -165,7 +165,7 @@ class nnUNetTrainer(object):
         self.num_epochs = 1000
         self.current_epoch = 0
         self.enable_deep_supervision = True
-        
+        """
 
         ### Dealing with labels/regions
         self.label_manager = self.plans_manager.get_label_manager(dataset_json)
@@ -520,11 +520,11 @@ class nnUNetTrainer(object):
             self.print_to_log_file('These are the global plan.json settings:\n', dct, '\n', add_timestamp=False)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.network.parameters(), self.initial_lr, weight_decay=self.weight_decay,momentum=0.99, nesterov=True)
-        #optimizer = torch.optim.Adam(self.network.parameters(),lr=self.initial_lr,weight_decay=self.weight_decay)
-        lr_scheduler = PolyLRScheduler(optimizer, self.initial_lr, self.num_epochs)
+        #optimizer = torch.optim.SGD(self.network.parameters(), self.initial_lr, weight_decay=self.weight_decay,momentum=0.99, nesterov=True)
+        optimizer = torch.optim.Adam(self.network.parameters(),lr=self.initial_lr,weight_decay=self.weight_decay)
+        #lr_scheduler = PolyLRScheduler(optimizer, self.initial_lr, self.num_epochs)
    
-        #lr_scheduler = PolyLRScheduler_offset(optimizer, self.initial_lr, self.num_epochs, self.num_epochs-10)
+        lr_scheduler = PolyLRScheduler_offset(optimizer, self.initial_lr, self.num_epochs, self.num_epochs-100)
         return optimizer, lr_scheduler
 
     def plot_network_architecture(self):
